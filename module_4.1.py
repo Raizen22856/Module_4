c7b98303350efcd180f9b9f3cbc52b9ad34b070b
Raizen22856 <assassincreedrevilations@gmail.com>
2025-01-08 13:33:31 +0300

    Add files via upload

---
+++ module_4.1.py
@@ -0,0 +1,10 @@
+from fake_math import divide as fm
+from true_math import divide as tm
+res1 = fm(75, 3)
+res2 = fm(35,0)
+res3 = tm(43, 5)
+res4 = tm(89, 0)
+print(res1)
+print(res2)
+print(res3)
+print(res4)
